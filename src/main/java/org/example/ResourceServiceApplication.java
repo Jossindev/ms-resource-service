3cbdab63e76868b7eec6a33bcdc475faeb196fe8
bboiko <79659123+bboiko-CLGX@users.noreply.github.com>
2023-06-04 13:57:51 +0300

    Add service registry

--- src/main/java/org/example/ResourceServiceApplication.java
+++ src/main/java/org/example/ResourceServiceApplication.java
@@ -2,9 +2,11 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableRetry
 public class ResourceServiceApplication {
 
