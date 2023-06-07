78cc90f61238c3b05e923a5f4f40118ce2b2a128
bboiko <79659123+bboiko-CLGX@users.noreply.github.com>
2023-06-07 12:36:32 +0300

    [TASK 5] Add service registration and API Gateway

---
+++ src/main/java/org/example/config/LoadBalancerConfiguration.java
@@ -0,0 +1,16 @@
+package org.example.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class LoadBalancerConfiguration {
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
