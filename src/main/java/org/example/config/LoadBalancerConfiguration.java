fae1d1d2e9395588e5c10049aa71abd3131f500d
bboiko <79659123+bboiko-CLGX@users.noreply.github.com>
2023-07-03 18:34:46 +0300

    Add tracing

--- src/main/java/org/example/config/LoadBalancerConfiguration.java
+++ src/main/java/org/example/config/LoadBalancerConfiguration.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import feign.Capability;
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
+
 @Configuration
 public class LoadBalancerConfiguration {
 
@@ -12,5 +16,10 @@ public class LoadBalancerConfiguration {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public Capability capability(final MeterRegistry registry) {
+        return new MicrometerCapability(registry);
     }
 }
